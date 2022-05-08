c1fa916327b274e21a97aff650430f6c507115f5
Jakub Czajka <CzajkaPL@o2.pl>
2022-05-08 19:53:54 +0200

    Changed labels layout, version 1.0

--- app/src/main/java/com/example/reversepolishnotationcalculator/CalculatorView.kt
+++ app/src/main/java/com/example/reversepolishnotationcalculator/CalculatorView.kt
@@ -1,7 +1,6 @@
 package com.example.reversepolishnotationcalculator
 
 import android.content.Intent
-import android.content.pm.ActivityInfo
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
